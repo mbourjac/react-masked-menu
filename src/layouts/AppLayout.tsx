34c74937a3f30252c76ca1eaa505b42c74e011ea
Michaël Bourjac <michael.bourjac@gmail.com>
2024-02-05 19:00:52 +0100

    feat: restore scroll on page navigation

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import { MaskedMenu } from '../components/MaskedMenu/MaskedMenu';
 
 export const AppLayout = () => {
   return (
-    <MaskedMenu>
-      <div className="min-h-screen p-[--main-padding]">
-        <main className="flex flex-col gap-36">
-          <Outlet />
-        </main>
-      </div>
-    </MaskedMenu>
+    <>
+      <ScrollRestoration />
+      <MaskedMenu>
+        <div className="min-h-screen p-[--main-padding]">
+          <main className="flex flex-col gap-36">
+            <Outlet />
+          </main>
+        </div>
+      </MaskedMenu>
+    </>
   );
 };
